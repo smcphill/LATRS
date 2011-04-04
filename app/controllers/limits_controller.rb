b1a867bd3a23a46bfb274114ae4fc5ff9d45d4de
Steven McPhillips <steven.mcphillips@gmail.com>
2011-04-04 16:28:11 +0700

    field limit options can now be set as default

--- app/controllers/limits_controller.rb
+++ app/controllers/limits_controller.rb
@@ -2,6 +2,16 @@ class LimitsController < ApplicationController
   active_scaffold :limits do | config |
     config.list.always_show_search = false
     config.actions.exclude  :search    
-    config.columns = :name, :field
+    config.columns = :name, :is_default, :field
+  end
+
+  def after_update_save(record)
+    if (record.is_default)
+      Limit.all(:conditions => 
+                ["field_id = ? AND id != ?", record.field_id, record.id]).each do |l|
+        l.is_default = false
+        l.save
+      end
+    end
   end
 end
