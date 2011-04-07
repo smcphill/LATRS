9c2bb8e720a2aad62fb47ebd64b3eceeb096fb93
Steven McPhillips <steven.mcphillips@gmail.com>
2011-04-07 14:38:39 +0700

    Added model validation for templates: names are unique, and forms can only be activated when they have at least 1 field attached

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,8 +1,19 @@
 class Template < ActiveRecord::Base
+  validate :validate_active_status
+  validates_uniqueness_of :name, 
+                          :message => "Form names must be unique", 
+                          :case_sensitive => false
+
   has_many :ancestors, :class_name => "Link", :foreign_key => "descendant_id"
   has_many :descendants, :class_name => "Link", :foreign_key => "ancestor_id"
   has_many :fields, :dependent => :destroy
   def to_label
     "#{name}"
+  end
+
+  def validate_active_status
+    if (is_active and fields.count() == 0)
+      errors.add (:fields, ": active forms require at least one data field.")
+    end
   end
 end
