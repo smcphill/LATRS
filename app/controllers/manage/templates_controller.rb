d3f0c05bb485fc794f73f5c651ef1fbbddb4234a
Steven McPhillips <steven.mcphillips@gmail.com>
2011-05-02 17:52:49 +0700

    Added a colour field to form templates, using a nifty colour picker for the view whch was fun, but a bit hacky. ideally i would be creating an AS bridge (a la :paperclip), but I'm lazy...

--- app/controllers/manage/templates_controller.rb
+++ app/controllers/manage/templates_controller.rb
@@ -11,11 +11,11 @@ class Manage::TemplatesController < ApplicationController
     config.create.action_after_create = 'show'
 
     #column definitions
-    config.columns = :name, :is_active, :fields, :ancestors, :descendants
+    config.columns = :name, :colour, :is_active, :fields, :ancestors, :descendants
     config.list.columns = :name, :is_active, :fields, :descendants
-    config.show.columns = :name, :is_active, :fields, :descendants
-    config.create.columns = :name
-    config.update.columns = :name, :is_active    
+    config.show.columns = :name, :is_active, :colour, :fields, :descendants
+    config.create.columns = :name, :colour
+    config.update.columns = :name, :colour, :is_active    
 
     #associations
     config.columns[:fields].clear_link
@@ -24,7 +24,7 @@ class Manage::TemplatesController < ApplicationController
     config.columns[:descendants].associated_limit = 10
     config.columns[:descendants].actions_for_association_links = [:show]
     config.nested.add_link("Form Fields", :fields)    
-    config.nested.add_link("Sub-tests", :descendants)    
+    config.nested.add_link("Sub-tests", :descendants)
 
     #labels
     config.columns[:name].label = "Form Name"
@@ -34,10 +34,11 @@ class Manage::TemplatesController < ApplicationController
     
     #form overrides
     config.columns[:is_active].inplace_edit = true
-    config.columns[:is_active].form_ui = :true
+    config.columns[:colour].inplace_edit = :ajax
 
     #descriptions
     config.columns[:name].description = "The name of the form"
+    config.columns[:colour].description = "The data entry form for this test will be this colour"
     config.columns[:is_active].description = "If selected, this form will be available for people to use"
     config.columns[:fields].description = "Fields to be shown in the form"
     config.columns[:descendants].description = "These forms will be displayed upon completion of this form, pre-populated with patient, tester, department and source data"
@@ -48,4 +49,12 @@ class Manage::TemplatesController < ApplicationController
     redirect_to :controller => '/manage', :action => 'index'
   end
 
+  def render_field
+    if (params[:column] == "colour" &&
+        params[:in_place_editing] == "true")
+      render :partial => "inplace_colour", :locals => {:record => Template.find(params[:id]) }
+    else
+      super
+    end
+  end
 end
