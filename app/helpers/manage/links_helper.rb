041c09488ff9b4bb8805584079d9b0f394fb7af4
Steven McPhillips <steven.mcphillips@gmail.com>
2011-05-03 16:38:38 +0700

    limit form sub-test candidate selection to forms that are not already sub-tests

--- app/helpers/manage/links_helper.rb
+++ app/helpers/manage/links_helper.rb
@@ -1,7 +1,10 @@
 module Manage::LinksHelper
   def link_ancestor_form_column(record, options)
+    template = Template.find(record.ancestor_id)
+    tests = template.descendants.collect{|x| x.descendant_id}
+    tests << record.ancestor_id
     select_tag("ancestor-input", 
-               options_from_collection_for_select(Template.all(:conditions => ["id != ?", record.ancestor_id]),
+               options_from_collection_for_select(Template.all(:conditions => ["id NOT IN (?)", tests]),
                                                   :id, :name), options)
   end
 end
