68a8ddaf143551f9f54693e12c2f6766353611e2
Steven McPhillips <steven.mcphillips@gmail.com>
2011-05-12 15:16:00 +0700

    movign to db sessions... cookies are too small apparently

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110512073555) do
+ActiveRecord::Schema.define(:version => 20110512081510) do
 
   create_table "departments", :force => true do |t|
     t.string   "name"
@@ -54,6 +54,16 @@ ActiveRecord::Schema.define(:version => 20110512073555) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "sources", :force => true do |t|
     t.string   "name"
